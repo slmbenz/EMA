5706d9e7c31e78b5e86e0bce917eb3196ed6fd9c
Abdessalem <abdessalem_benzarti@hotmail.fr>
2018-06-26 17:02:50 +0200

    create

---
+++ app/src/main/java/com/example/abdessalem/realtimetrack/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.abdessalem.realtimetrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    private FirebaseAuth mAuth;
+    private static final int LOGIN_PERMISSION=1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnLogin = (Button)findViewById(R.id.btnSignIn);
+        mAuth = FirebaseAuth.getInstance();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                        AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAllowNewEmailAccounts(true).build(),LOGIN_PERMISSION
+                );
+            }
+        });
+    }
+}
